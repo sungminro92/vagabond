75f1da87b7b79300943285aa9ea97b26580cb502
tjdals504 <sungminro92@gmail.com>
2017-04-27 08:46:58 -0400

    Added relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => 'cities#index'
+  get '/' => 'cities#index' # list all cities page
+  get '/cities' => 'cities#show', as: 'city' # city show page
+
   # get '/cities/:id' => 'cities#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
