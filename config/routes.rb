49cae798b3b6d06448d478123056b5cd76502143
tjdals504 <sungminro92@gmail.com>
2017-04-28 10:13:08 -0400

    Edited layout page nav BUT delete & redirect & seed not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-  # resources :posts this provides all crud actions?
   # resources :cities do
-      # resource :posts
-  # end
+  #     resource :posts
+  # end  ----> THIS WILL DO ALL ROUTES WELL.
+
+  devise_for :users
+  get '/' => 'cities#index' # list all cities page
+
+  get '/cities' => 'cities#index', as: 'city'
+  get '/cities/:id' => 'cities#show', as: 'show'
+
   get '/cities/:city_id/posts/new' => 'posts#new', as: 'new'
 
   post '/cities/:city_id' => 'posts#create'
@@ -16,14 +22,10 @@ Rails.application.routes.draw do
 
   delete '/cities/:id' => 'posts#destroy', as: 'post'
 
-  devise_for :users
-  get '/' => 'cities#index' # list all cities page
-  get '/cities' => 'cities#index', as: 'city'
 
 
-
-  get '/cities/:id' => 'cities#show', as: 'show'
-
+  # ADDED for root
+  root 'cities#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@ City.destroy_all
 Post.destroy_all
 
 
-
 atlanta = City.create(name: 'Atlanta', image_url:'http://i.imgur.com/uEyWAwc.png')
 new_york = City.create(name: 'New York', image_url:'http://i.imgur.com/rbQlY3F.png')
 seoul = City.create(name: 'Seoul', image_url:'http://i.imgur.com/tAmZruh.png')
@@ -20,15 +19,15 @@ austin = City.create(name: 'Austin', image_url:'http://i.imgur.com/8tqtGSy.png')
 barcelona = City.create(name: 'Barcelona', image_url:'http://i.imgur.com/B7IppDk.png')
 seattle = City.create(name: 'Seattle', image_url:'http://i.imgur.com/Ec6cea9.png')
 
-user = User.create(
-  email: 'user@domain.com',
-  password: '123456789',
-  password_confirmation: '123456789'
-)
+# user = User.create(
+#   email: 'user@domain.com',
+#   password: '123456789',
+#   password_confirmation: '123456789'
+# )
 
 
 Post.create( content: " Go roller skating at Cascade Fun Center
-They shot ATL there! Also, every year they have events that bring all sorts of folks out to slide around on that slick, shiny floor. Word of advice: you might bust your ass trying that old reverse-in-motion trick you used to do in middle school, so don’t get too turnt", city_id: atlanta.id, user_id: user.id )
+They shot ATL there! Also, every year they have events that bring all sorts of folks out to slide around on that slick, shiny floor. Word of advice: you might bust your ass trying that old reverse-in-motion trick you used to do in middle school, so don’t get too turnt", city_id: atlanta.id, user_id: user.id: 1 )
 
 Post.create( content: "We love visiting New York City! When we visit, we spend most our time eating, since the options for food are endless. It wasn’t until recently that we realized that we haven’t seen much of the city other than the insides of restaurants. With the help of our friend Charlene, who’s been living there for 10 years, we were able to put together an NYC bucket list.", city_id: new_york.id, user_id:user.id )
 
