7e6472ed5fdce059324f7d23fb6139fa1064e04c
tjdals504 <sungminro92@gmail.com>
2017-05-01 12:25:48 -0400

    Some css change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
+  # Layout index
   def index
   
   end
 
+  # User profile
   def show
     @user = current_user
     @user_posts =  @user.posts
   end
 
+  # About us page
   def about
   
   end
